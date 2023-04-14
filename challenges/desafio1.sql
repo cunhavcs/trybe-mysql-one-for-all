0009338517b5724c0fd07858646ea99772560f0f
cunhavcs <cunha.vcs@gmail.com>
2023-04-14 18:52:34 -0300

    feat(desafio1): cria e insere registros na tabela "pessoa_usuaria"

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -2,18 +2,30 @@ DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE IF NOT EXISTS SpotifyClone;
 
-CREATE TABLE SpotifyClone.plano (
+USE SpotifyClone
+
+CREATE TABLE plano (
     plano_id INT PRIMARY KEY AUTO_INCREMENT,
     nome_plano VARCHAR(32) NOT NULL,
     valor_plano DECIMAL(5,2) NOT NULL,
 ) engine = InnoDB;
 
-CREATE TABLE SpotifyClone.pessoa_artista (
+CREATE TABLE pessoa_artista (
     pessoa_artista_id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_pessoa_artista VARCHAR(32)
+    nome_pessoa_artista VARCHAR(32) NOT NULL
 ) engine = InnoDB;
 
-INSERT INTO SpotifyClone.plano (
+CREATE TABLE pessoa_usuaria (
+    pessoa_usuaria_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_pessoa_usuaria VARCHAR(32) NOT NULL,
+    sobrenome_pessoa_usuaria VARCHAR(32) NOT NULL,
+    idade INT NOT NULL,
+    plano_id INT NOT NULL,
+    data_assinatura DATE NOT NULL,
+    FOREIGN KEY (plano_id) REFERENCES plano(plano_id)
+) engine = InnoDB;
+
+INSERT INTO plano (
     nome_plano,
     valor_plano
 ) VALUES
@@ -22,7 +34,7 @@ INSERT INTO SpotifyClone.plano (
     ('pessoal', 6.99),
     ('familiar', 7.99);
 
-INSERT INTO SpotifyClone.pessoa_artista (
+INSERT INTO pessoa_artista (
     nome_pessoa_artista
 ) VALUES
     ('Baco Exu do Blues'),
@@ -31,3 +43,22 @@ INSERT INTO SpotifyClone.pessoa_artista (
     ('Elis Regina')
     ('Nina Simone')
     ('Queen');
+
+INSERT INTO pessoa_usuaria (
+    pessoa_usuaria_id,
+    nome_pessoa_usuaria,
+    sobrenome_pessoa_usuaria,
+    idade,
+    plano_id,
+    data_assinatura,
+) VALUES
+    ('Barbara', 'Liskov', 82, 1, '2019-10-20'),
+    ('Robert', 'Cecil Martin', 58, 1, '2017-01-06'),
+    ('Ada', 'Lovelace', 37, 4, '2017-12-30'),
+    ('Martin', 'Fowler', 46, 4, '2017-01-17'),
+    ('Sandi', 'Metz', 58, 4, '2018-04-29'),
+    ('Paulo', 'Freire', 19, 2, '2018-02-14'),
+    ('Bell', 'Hooks', 26, 2, '2018-01-05'),
+    ('Christopher', 'Alexander', 85, 3, '2019-06-05'),
+    ('Judith', 'Butler', 45, 3, '2020-05-13'),
+    ('Jorge', 'Amado', 58, 3, '2017-02-17');
