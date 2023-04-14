c87551d954b2dd75d147a1053497b821c3d08cf4
cunhavcs <cunha.vcs@gmail.com>
2023-04-14 19:07:33 -0300

    feat(desafio1): cria e insere registros na tabela "album"

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -33,6 +33,14 @@ CREATE TABLE seguindo_pessoa_artista (
     FOREIGN KEY (pessoa_artista_id) REFERENCES pessoa_artista(pessoa_artista_id)
 );
 
+CREATE TABLE album (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_album VARCHAR(32) NOT NULL,
+    pessoa_artista_id	INT NOT NULL,
+    ano_lancamento_album INT NOT NULL,
+    FOREIGN KEY (pessoa_artista_id) REFERENCES pessoa_artista(pessoa_artista_id)
+);
+
 INSERT INTO plano (
     nome_plano,
     valor_plano
@@ -76,17 +84,32 @@ INSERT INTO seguindo_pessoa_artista (
     pessoa_usuaria_id,
     pessoa_artista_id
 ) VALUES
-    ('1', '2'),
-    ('1', '6'),
-    ('1', '4'),
-    ('2', '2'),
-    ('2', '4'),
-    ('3', '6'),
-    ('4', '1'),
-    ('5', '3'),
-    ('5', '5'),
-    ('6', '5'),
-    ('6', '2'),
-    ('7', '5'),
-    ('9', '4'),
-    ('10', '6');
+    (1, 2),
+    (1, 6),
+    (1, 4),
+    (2, 2),
+    (2, 4),
+    (3, 6),
+    (4, 1),
+    (5, 3),
+    (5, 5),
+    (6, 5),
+    (6, 2),
+    (7, 5),
+    (9, 4),
+    (10, 6);
+
+INSERT INTO album (
+    album_id,
+    nome_album,
+    pessoa_artista_id,
+    ano_lancamento_album
+) VALUES
+    ('Renaissance', 2, 2022),
+    ('Jazz', 6, 1978),
+    ('Hot Space', 6, 1982),
+    ('Falso Brilhante', 4, 1998),
+    ('Vento de Maio', 4, 2001),
+    ('QVVJFA?', 1, 2003),
+    ('Somewhere Far Beyond', 3, 2007),
+    ('I Put A Spell On You', 5, 2012);
