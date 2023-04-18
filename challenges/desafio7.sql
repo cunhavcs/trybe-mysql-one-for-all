4dae4aff9342b4cee4c07381c68a5260140b459d
cunhavcs <cunha.vcs@gmail.com>
2023-04-18 14:46:43 -0300

    feat(desafio7.sql): exibe relação de álbuns por artista e os seguidores destes
    Criar uma query que exibe uma tabela em que a coluna "artista" contém o nome dos artistas, a coluna "album" contém o nome dos álbuns e a coluna "pessoas_seguidoras" contém a quantidade de seguidores do artista.

---
+++ challenges/desafio7.sql
@@ -0,0 +1,14 @@
+SELECT
+	  ar.artista,
+    al.album,
+    COUNT(sa.usuario_id) AS pessoas_seguidoras
+FROM artistas AS ar
+INNER JOIN albuns AS al
+	  ON ar.artista_id = al.artista_id
+INNER JOIN seguidores_artistas AS sa
+	  ON ar.artista_id = sa.artista_id
+GROUP BY ar.artista, al.album
+ORDER BY
+	  pessoas_seguidoras DESC,
+    ar.artista,
+    al.album;
